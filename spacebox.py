38a6682d85706d3911ad411711c4867bf019be22
Kunal Sahu <kunalsahu@kunalsahu-macbookair.roam.corp.google.com>
2014-08-04 16:06:35 -0700

    Second major revision

--- spacebox.py
+++ spacebox.py
@@ -6,14 +6,12 @@ import posixpath
 import Queue
 import sys
 
-from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack
 
 from dropbox.client import DropboxClient, DropboxOAuth2Flow
 
 DEBUG = True
-DATABASE = 'app.db'
 SECRET_KEY = 'development key'
 
 # Fill this in!
@@ -23,12 +21,6 @@ DROPBOX_APP_SECRET = 'g28lix8n4w26so4'
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-# Ensure db directory exists.
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
 
 def human_readable(bytes):
   if bytes < 1024:
@@ -145,41 +137,11 @@ def get_data_by_types(metadata, used_bytes):
     type_list.append([type, float((size * 100) / used_bytes)])
   return json.JSONEncoder().encode(type_list)
 
-def init_db():
-  with app.app_context():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-      db.cursor().executescript(f.read())
-    db.commit()
-
-def get_db():
-  top = _app_ctx_stack.top
-  if not hasattr(top, 'sqlite_db'):
-    sqlite_db = sqlite3.connect(
-        os.path.join(app.instance_path, app.config['DATABASE']))
-    sqlite_db.row_factory = sqlite3.Row
-    top.sqlite_db = sqlite_db
-  return top.sqlite_db
-
-def get_access_token():
-  username = session.get('user')
-  if username is None:
-      return None
-  db = get_db()
-  row = db.execute(
-      'SELECT access_token FROM users WHERE username = ?',
-      [username]).fetchone()
-  if row is None:
-    return None
-  return row[0]
-
 @app.route('/')
 def home():
-  if 'user' not in session:
-    return redirect(url_for('login'))
-  access_token = get_access_token()
   real_name = None
-  if access_token is not None:
+  if 'access_token' in session and session['access_token'] is not None:
+    access_token = session['access_token']
     client = DropboxClient(access_token)
     account_info = client.account_info()
     real_name = account_info['display_name']
@@ -187,9 +149,6 @@ def home():
 
 @app.route('/dropbox-auth-finish')
 def dropbox_auth_finish():
-  username = session.get('user')
-  if username is None:
-    abort(403)
   try:
     access_token, user_id, url_state = get_auth_flow().finish(request.args)
   except DropboxOAuth2Flow.BadRequestException, e:
@@ -204,28 +163,16 @@ def dropbox_auth_finish():
   except DropboxOAuth2Flow.ProviderException, e:
     app.logger.exception('Auth error' + e)
     abort(403)
-  db = get_db()
-  data = [access_token, username]
-  db.execute('UPDATE users SET access_token = ? WHERE username = ?', data)
-  db.commit()
+  session['access_token'] = access_token
   return redirect(url_for('home'))
 
 @app.route('/dropbox-auth-start')
 def dropbox_auth_start():
-  if 'user' not in session:
-    abort(403)
   return redirect(get_auth_flow().start())
 
 @app.route('/dropbox-logout')
 def dropbox_logout():
-  username = session.get('user')
-  if username is None:
-    abort(403)
-  db = get_db()
-  db.execute(
-      'UPDATE users SET access_token = NULL WHERE username = ?',
-      [username])
-  db.commit()
+  session['access_token'] = None
   return redirect(url_for('home'))
 
 def get_auth_flow():
@@ -233,37 +180,15 @@ def get_auth_flow():
   return DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET, redirect_uri,
                            session, 'dropbox-auth-csrf-token')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  error = None
-  if request.method == 'POST':
-    username = request.form['username']
-    if username:
-      db = get_db()
-      db.execute(
-          'INSERT OR IGNORE INTO users (username) VALUES (?)',
-          [username])
-      db.commit()
-      session['user'] = username
-      flash('You were logged in')
-      return redirect(url_for('home'))
-    else:
-      flash('You must provide a username')
-  return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-  session.pop('user', None)
-  flash('You were logged out')
-  return redirect(url_for('home'))
-
 @app.route('/dashboard')
 def dashboard():
-  if 'user' not in session:
-    return redirect(url_for('login'))
-  access_token = get_access_token()
+  if 'access_token' not in session or session['access_token'] is None:
+    return redirect(url_for('home'))
+  access_token = session['access_token']
   client = DropboxClient(access_token)
   used_bytes, quota_bytes = quota_info(client)
+  session['used_bytes'] = used_bytes
+  session['metadata'] = get_metadata(client, '')
   return render_template('dashboard.html',
                          used=human_readable(used_bytes),
                          quota=human_readable(quota_bytes),
@@ -271,26 +196,17 @@ def dashboard():
 
 @app.route('/contents')
 def contents():
-  if 'user' not in session:
-    return redirect(url_for('login'))
-  access_token = get_access_token()
-  client = DropboxClient(access_token)
-  used_bytes, _ = quota_info(client)
-  metadata = get_metadata(client, '')
-  return get_data_by_contents(metadata, used_bytes)
+  if 'access_token' not in session or session['access_token'] is None:
+    return redirect(url_for('home'))
+  return get_data_by_contents(session['metadata'], session['used_bytes'])
 
 @app.route('/types')
 def types():
-  if 'user' not in session:
-    return redirect(url_for('login'))
-  access_token = get_access_token()
-  client = DropboxClient(access_token)
-  used_bytes, _ = quota_info(client)
-  metadata = get_metadata(client, '')
-  return get_data_by_types(metadata, used_bytes)
+  if 'access_token' not in session or session['access_token'] is None:
+    return redirect(url_for('home'))
+  return get_data_by_types(session['metadata'], session['used_bytes'])
 
 def main():
-  init_db()
   app.run()
 
 if __name__ == '__main__':
